7f1c5e34cae50444f25943aa8ed7db49a94312f5
NCHlab <bt17684@qmul.ac.uk>
2018-01-13 00:47:13 +0000

    MySQL test code

--- Website_Design/project.py
+++ Website_Design/project.py
@@ -17,7 +17,7 @@ try:
 						port=3306,
 						user="root",
 						password="root",
-						db="",#genome_data
+						db="genome_data",#genome_data
 						charset="utf8mb4",
 						cursorclass=pymysql.cursors.DictCursor)
 except:
@@ -25,7 +25,6 @@ except:
 	print "Please ensure you have started your MySQL server and the db name is correct"
 	print "and that the port is set to 3306 in your server"
 	pass
-
 
 
 
@@ -64,6 +63,7 @@ def peptide_seq_ident():
 			filename = file.filename
 			destination = "/".join([target, filename])
 			file.save(destination)
+
 		return render_template("peptide_seq_ident.html")
 	else:
 		return render_template("peptide_seq_ident.html")
@@ -86,6 +86,25 @@ def upload_peptide():
 
 @app.route("/uploaded")
 def uploaded():
+	try:
+	    with connection.cursor() as cursor:
+	        # Create a new record
+			#cursor.execute("USE ")
+	        sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+	        cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+
+	    # connection is not autocommit by default. So you must commit to save
+    	# your changes.
+	    connection.commit()
+
+	    with connection.cursor() as cursor:
+	        # Read a single record
+	        sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+	        cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+	        result = cursor.fetchone()
+	        print(result)
+	finally:
+	    connection.close()
 	return render_template("uploaded.html")
 
 @app.route("/expression_atlas")
